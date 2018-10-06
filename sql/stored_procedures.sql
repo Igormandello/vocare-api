d0b5904e96b79ae2175023f0dc8c48c669abbe42
Igormandello <igormandello@gmail.com>
2018-10-06 11:34:42 -0300

    Starts creating the stored procedures
    - Creates a procedure to count user messages
    
    - Creates a procedure to check the login
    
    Co-Authored-By: Vitor Bartier <bartier@users.noreply.github.com>

---
+++ sql/stored_procedures.sql
@@ -0,0 +1,17 @@
+--Counts the total messages from an user
+--Messages = Posts + Comments
+CREATE PROCEDURE sp_count_messages
+	@user_id int
+AS
+ SELECT SUM(post.[count] + comment.[count]) AS [count] FROM
+ (SELECT COUNT(*) AS [count] FROM post WHERE user_id = @user_id) post,
+ (SELECT COUNT(*) AS [count] FROM comment WHERE user_id = @user_id) comment
+
+ ---------------------------------------------------------------------------
+
+--Returns the user who have that email and password
+CREATE PROCEDURE sp_user_login
+	@email varchar(255),
+	@password varchar(64)
+AS
+ SELECT * FROM [user] WHERE email = @email and password = @password
