facf030199c9b6d58af715c07f1ab5cfb7fdea04
Igor Mandello <igormandello@gmail.com>
2018-10-07 00:18:17 -0300

    Creates a overload for login and a procedure to register a social media
    - The overload accepts a provider and a access token and returns the user
    
    - The sp_register_user_media accepts a provider, an access token, an id and an optional username. If the ID specified is 0, a new user named username will be created and the provider will be linked to him, if the id isn't 0, the provider will be linked to the user with that id

--- sql/stored_procedures.sql
+++ sql/stored_procedures.sql
@@ -15,3 +15,43 @@ CREATE PROCEDURE sp_user_login
 	@password varchar(64)
 AS
  SELECT * FROM [user] WHERE email = @email and password = @password
+ 
+ ---------------------------------------------------------------------------
+
+--Returns the user who have that access token
+CREATE PROCEDURE sp_user_login_provider
+	@access_token varchar(max),
+	@provider varchar(50)
+AS
+ SELECT * FROM [user] WHERE id in (
+    SELECT user_id FROM user_login WHERE access_token = @access_token AND provider_id = (
+        SELECT id FROM provider WHERE name = @provider
+    )
+ )
+ 
+---------------------------------------------------------------------------
+
+--Register a provider as a user media
+CREATE PROCEDURE sp_register_user_media
+	@provider varchar(50),
+	@access_token varchar(max),
+	@id int,
+	@username AS varchar(30) = ''
+AS
+ IF EXISTS(SELECT * FROM user_login WHERE access_token = @access_token AND provider_id = (
+    SELECT id FROM provider WHERE name = @provider
+ ))
+ BEGIN
+    RAISERROR ('%d: %s', 16, 1, 100, 'This account is already registered')
+    RETURN
+ END
+    
+ IF @id <= 0
+ BEGIN
+    INSERT INTO [user] VALUES (NULL, NULL, @username, 1, NULL)
+    INSERT INTO user_login VALUES ((SELECT MAX(id) AS user_id FROM [user]), @access_token, (SELECT id as prover_id FROM provider WHERE name = @provider))
+ END
+ ELSE
+    INSERT INTO user_login VALUES (@id, @access_token, (SELECT id AS prover_id FROM provider WHERE name = @provider))
+ 
+---------------------------------------------------------------------------
