8ad696f0e4a7a5dd4feb6cab82f9de73a339c2fe
Igormandello <igormandello@gmail.com>
2018-10-15 12:54:20 -0300

    Removes test controller reference

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var app = express();
 
-var postController = require('./src/controllers/postController');
-app.use('/api/post', postController);
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
