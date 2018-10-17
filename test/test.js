f1db8b0338ba386effe7177ab72d4d498f560059
Igormandello <igormandello@gmail.com>
2018-10-17 13:49:28 -0300

    Updates the test.js and ContainerManager.js
    - ContainerManager now stores the connection and have a execSql method to send it to the container server
    
    - Before the Mocha starts, the database files are read and the table/foreign keys/hard data/stored procedures are created

--- test/test.js
+++ test/test.js
@@ -1,7 +1,35 @@
+const fs = require('fs');
 const ContainerManager = require('./ContainerManager');
 
 let cm = new ContainerManager();
-cm.createContainer().then(() => run());
+cm.createContainer().then(async () => {
+	console.log('\nCreating tables...');
+	let sqlScript = fs.readFileSync('./sql/bd.sql', 'utf8');
+	await cm.execSql(sqlScript);
+	console.log('Tables created');
+
+	console.log('\nSetting foreign keys...');
+	sqlScript = fs.readFileSync('./sql/foreign_keys.sql', 'utf8');
+	await cm.execSql(sqlScript);
+	console.log('Foreign keys created');
+
+	//console.log('\nInserting default data...');
+	//sqlScript = fs.readFileSync('./sql/hard_data.sql', 'utf8');
+	//await cm.execSql(sqlScript);
+
+	console.log('\nCreating stored procedures...');
+	sqlScript = fs.readFileSync('./sql/stored_procedures.sql', 'utf8');
+	sqlScript = sqlScript.split('GO');
+
+	let procedures = [];
+	for (let i = 0; i < sqlScript.length; i++)
+		procedures.push(cm.execSql(sqlScript[i]));
+
+	await Promise.all(procedures);
+	console.log('Foreign keys created');
+	
+	run();
+});
 
 describe('Api health tests', () => {
 	it('should return status 200', function (done) {
@@ -14,6 +42,6 @@ describe('Api health tests', () => {
 	});
 });
 
-after(async () => {
-	await cm.deleteContainer();
+after(() => {
+	return cm.deleteContainer()
 });
