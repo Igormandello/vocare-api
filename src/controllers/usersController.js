f6317dc4ac8e0046b0236bf415238814a942ef59
Igor Mandello <igormandello@gmail.com>
2018-10-18 23:45:26 -0300

    Adds the 'POST /' in usersController
    - Adds sha256 package to encrypt the password
    
    - Implements the insertion of an user with email and password or with a social media

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sha256 = require('sha256');
+const mssql = require('mssql');
 const { runSql } = require('../db');
 
 router.get('/', (req, res) => {
@@ -8,7 +10,32 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  res.status(501).send('POST response');
+  if (req.body.email && req.body.password)
+    runSql('exec sp_register_user @email, @password, @username',
+      { name: 'email', type: mssql.VarChar(255), value: req.body.email },
+      { name: 'password', type: mssql.VarChar(64), value: sha256(req.body.password) },
+      { name: 'username', type: mssql.VarChar(30), value: req.body.username }
+    ).then((result) => {
+      result = result.recordset[0];
+      res.status(201).send({
+        id: result.id,
+        username: result.username,
+        profile_picture: result.profile_picture
+      });
+    }).catch(e => res.status(400).send(e));
+  else 
+    runSql('exec sp_register_user_media @provider, @access_token, -1, @username',
+      { name: 'provider', type: mssql.VarChar(50), value: req.body.provider },
+      { name: 'access_token', type: mssql.VarChar(mssql.MAX), value: req.body.access_token },
+      { name: 'username', type: mssql.VarChar(30), value: req.body.username }
+    ).then((result) => {
+      result = result.recordset[0];
+      res.status(201).send({
+        id: result.id,
+        username: result.username,
+        profile_picture: result.profile_picture
+      });
+    }).catch(e => res.status(400).send(e));
 });
 
 router.put('/', (req, res) => {
