70a7fd84203b173b0bcf19e237e1d34dfdbf3a8f
Igormandello <igormandello@gmail.com>
2018-10-23 09:46:55 -0300

    Implements commentsController

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use('/api/users', require('./controllers/usersController'));
 app.use('/api/posts', require('./controllers/postsController'));
 app.use('/api/tags', require('./controllers/tagsController'));
 app.use('/api/areas', require('./controllers/areasController'));
+app.use('/api/comments', require('./controllers/commentsController'));
 app.use('/api/health', require('./health'));
 
 const port = process.env.PORT || 3000;
