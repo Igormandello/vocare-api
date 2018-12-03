39cbef53d7a4d356b56a57183db5b4a770567a40
Igormandello <igormandello@gmail.com>
2018-12-03 14:54:43 -0200

    Raises the API limit to 2MB

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ global.errors = [
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '2MB', type:'application/json'}));
 app.use(require('./middleware/auth').checkAuth);
 
 app.use('/api/users', require('./controllers/usersController'));
